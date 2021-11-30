37418fe4445a0682f35d98291a9a25e0600d443a
Alexey Vasiliev <a.p.vasiliev@yandex.ru>
2021-12-01 01:38:26 +0300

    initial

--- distribute.py
+++ distribute.py
@@ -1,24 +1,73 @@
 #!/usr/bin/env python3 
 
+from datetime import datetime
+from pathlib import Path
 import argparse
 import exifread
+import logging
 import os
+import sys
+
+
+log = logging.getLogger(__name__)
+
+
+def _get_exif_date(image_path):
+    with open(image_path, 'rb') as image_file:
+        tags = exifread.process_file(image_file)
+    datetime_string = tags.get('EXIF DateTimeOriginal') or tags.get('Image DateTime')
+    if not datetime_string:
+        return None
+    if str(datetime_string).startswith('0000'):
+        return None
+    try:
+        return datetime.strptime(str(datetime_string), '%Y:%m:%d %H:%M:%S')
+    except ValueError:
+        return None
+
+
+def _create_directory_for_date(destination_path, date):
+    directory = Path(destination_path, f'{date.year}', f'{date.month:02d}')
+    Path(directory).mkdir(parents=True, exist_ok=True)
+    return directory
+
+
+def _make_hardlink(source_path, destination_path):
+    log.info(f'{source_path!s} <- {destination_path!s}')
+    source_path.link_to(destination_path)
 
 
 def distribute(source_path, destination_path):
-    source_path = os.path.abspath(source_path)
-    destination_path = os.path.abspath(destination_path)
-    assert source_path not in destination_path and destination_path not in source_path
+    source_path = Path(source_path).resolve()
+    destination_path = Path(destination_path).resolve()
+    log.info(f'{source_path=!s}')
+    log.info(f'{destination_path=!s}')
+
+    try:
+        source_path.relative_to(destination_path)
+    except ValueError:
+        pass
+    else:
+        raise Exception('Source and destination paths are relative')
     
-    for directory, _, file_names in os.walk():
+    for directory, _, file_names in os.walk(source_path):
         for file_name in file_names:
-            if os.path.splitext(file_name)[1].lower() not in ('.jpg', '.jpeg'):
+            if Path(file_name).suffix.lower() not in ('.jpg', '.jpeg'):
                 continue
-            file_path = os.path.join(directory, file_name)
-            _get_exif_date()
+            file_path = Path(directory, file_name)
+            exif_datetime = _get_exif_date(file_path)
+            if exif_datetime is None:
+                log.warning(f'No datetime found for {file_path!s}')
+                continue
+            directory = _create_directory_for_date(destination_path, exif_datetime.date())
+            datetime_file_name = '{}.jpg'.format(exif_datetime.isoformat())
+            _make_hardlink(file_path, Path(directory, datetime_file_name))
+            raise Exception('Enough')
 
 
 def main():
+    logging.basicConfig(stream=sys.stderr, level=logging.INFO, format='%(msg)s')
+
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument('--source-path', required=True)
     argument_parser.add_argument('--destination-path', required=True)
